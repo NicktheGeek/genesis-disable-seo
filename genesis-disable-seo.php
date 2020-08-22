b6a283afac3c5620aa6a3179d7d27809255fb884
Nick Croft <nick@reaktivstudios.com>
2020-08-22 16:22:58 -0400

    Update genesis-disable-seo.php

--- genesis-disable-seo.php
+++ genesis-disable-seo.php
@@ -4,7 +4,7 @@
  * Author: NicktheGeek
  * Author URI: http://nickexplains.com
  * Description: Disables Genesis SEO.
- * Version: 1.1.0
+ * Version: 1.0.0
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  *
