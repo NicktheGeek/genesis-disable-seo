df258796161972354d5c1da00e3ca55a42977e81
Nick Croft <nick@reaktivstudios.com>
2020-08-22 16:18:43 -0400

    Create genesis-disable-seo.php

---
+++ genesis-disable-seo.php
@@ -0,0 +1,15 @@
+/**
+ * Plugin Name: Genesis Disable SEO
+ * Plugin URI: https://github.com/NicktheGeek/genesis-disable-seo/
+ * Author: NicktheGeek
+ * Author URI: http://nickexplains.com
+ * Description: Disables Genesis SEO.
+ * Version: 1.1.0
+ * License: GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * @package genesis-disable-seo
+ */
+ 
+ add_action( 'genesis_init', 'genesis_disable_seo', 99 );
+ 
